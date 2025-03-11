05259ef2df8a8697232675c8d3ab0f4434ef4b59
DarkDna74 <tommaso.guglielmi@gmail.com>
2025-03-11 17:51:33 +0100

    Update save.js

--- api/save.js
+++ api/save.js
@@ -3,14 +3,32 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
-const { user, error } = await supabase.auth.signInWithPassword({
+
+
+async function login() {
+  try {
+  const { user, error } = await supabase.auth.signInWithPassword({
   email: "tommaso.guglielmi@gmail.com",
   password: "XdragoSupa21!",
 });
+
+    if (error) {
+      console.error("Errore di login:", error.message);
+    } else {
+      console.log("Login riuscito:", user);
+    }
+  } catch (err) {
+    console.error("Errore:", err);
+  }
+}
+
+
+
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
+    login();
 
     const { session, longest_word, first_sentence } = req.body;
 
