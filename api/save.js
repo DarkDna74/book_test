7eceb263cb120dacdbe4596bae042a8f223c5b52
DarkDna74 <tommaso.guglielmi@gmail.com>
2025-03-21 17:12:04 +0100

    Update save.js

--- api/save.js
+++ api/save.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
-
+console.log(req.body); 
     const user = await login();  // Aspetta che il login sia completato
 
     if (!user) {
